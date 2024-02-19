9ca315e362a2fdcef75a002d3bc514730e9f2484
Florian Kotthoff <florian.kotthoff@posteo.de>
2024-02-19 17:34:37 +0100

    Add test for triangle area

--- tests/triangleUtils.test.ts
+++ tests/triangleUtils.test.ts
@@ -2,16 +2,21 @@ import {describe, expect, test} from 'vitest';
 import * as triangleUtils from '../src/triangleUtils.js';
 import { vec3 } from "gl-matrix";
 
-const triangleArray = new Float32Array([0.,0.,0.,1.,0.,0.,0.,1.,0.]); 
+const triangleArray = new Float32Array([0.,0.,0.,1.,0.,0.,0.,1.,0.,1.,1.,1.,1.,3.,1.,1.,3.,6.]); 
 const firstTriangle = [vec3.fromValues(0.,0.,0.), vec3.fromValues(1.,0.,0.), vec3.fromValues(0.,1.,0.)]
+const secondTriangle = [vec3.fromValues(1.,1.,1.), vec3.fromValues(1.,3.,1.), vec3.fromValues(1.,3.,6.)]
 
 describe('Triangle Util functions: ', () => {
     test('ectract triangle from array', () => {
       expect(triangleUtils.extractTriangle(triangleArray, 0)).toStrictEqual(firstTriangle);
+      expect(triangleUtils.extractTriangle(triangleArray, 9)).toStrictEqual(secondTriangle);
+    });
+    test('calculate area of triangles', () => {
+      expect(triangleUtils.area(triangleArray,0)).to.equal(0.5);
+      expect(triangleUtils.area(triangleArray,9)).to.equal(5);
     });
   });
 
 // TODO:
-// * Test area
 // * Test normals
 // * Test subdivision
