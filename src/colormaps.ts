3b1d4fb16bd7e0d1528ac7b06019dbab284d3a66
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-04-06 08:39:57 +0200

    Delete unnecessary comments

--- src/colormaps.ts
+++ src/colormaps.ts
@@ -1,7 +1,6 @@
-// from https://observablehq.com/@flimsyhat/webgl-color-maps
 export function viridis(t: number) {
   t = Math.min(Math.max(t, 0), 1);
-  //viridis
+  //viridis from https://observablehq.com/@flimsyhat/webgl-color-maps
   const c0 = [0.2777273272234177, 0.005407344544966578, 0.3340998053353061];
   const c1 = [0.1050930431085774, 1.404613529898575, 1.384590162594685];
   const c2 = [-0.3308618287255563, 0.214847559468213, 0.09509516302823659];
@@ -9,8 +8,6 @@ export function viridis(t: number) {
   const c4 = [6.228269936347081, 14.17993336680509, 56.69055260068105];
   const c5 = [4.776384997670288, -13.74514537774601, -65.35303263337234];
   const c6 = [-5.435455855934631, 4.645852612178535, 26.3124352495832];
-
-  //const res = c0*0.004+t*(c1+t*(c2+t*(c3+t*(c4+t*(c5+t*c6)))))*0.004;
   const res = [
     c0[0] + t * (c1[0] + t * (c2[0] + t * (c3[0] + t * (c4[0] + t * (c5[0] + t * c6[0]))))),
     c0[1] + t * (c1[1] + t * (c2[1] + t * (c3[1] + t * (c4[1] + t * (c5[1] + t * c6[1]))))),
