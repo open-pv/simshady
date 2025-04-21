9a89f7f7801bbb9cc033fb4cefdf463b1459a7d7
Florian Kotthoff <florian.kotthoff@posteo.de>
2025-04-21 12:23:42 +0200

    Add tests for main script #47

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -23,11 +23,83 @@ describe('Scene refineMesh', () => {
 
   test('does not subdivide triangle edges shorter than or equal to maxLength', () => {
     const scene = new ShadingScene();
+    const positions = [0, 0, 0, 1, 0, 0, 0, 1, 0]; // Diagonal longer than 1
+    const normals = [0, 0, 1, 0, 0, 1, 0, 0, 1];
+    const testGeometry = createTestGeometry(positions, normals);
+    const refinedGeometry = scene.refineMesh(testGeometry, 1);
+    expect(refinedGeometry.attributes.position.count).toEqual(6); //Expect one subdivision
+  });
+});
+
+describe('Scene initialization', () => {
+  test('addShadingGeometry works', () => {
+    const scene = new ShadingScene();
     const positions = [0, 0, 0, 1, 0, 0, 0, 1, 0];
     const normals = [0, 0, 1, 0, 0, 1, 0, 0, 1];
     const testGeometry = createTestGeometry(positions, normals);
-    const refinedGeometry = scene.refineMesh(testGeometry, 1.5);
+    scene.addShadingGeometry(testGeometry);
+    expect(scene.shadingGeometry?.attributes.position.count).toEqual(3);
+  });
 
-    expect(refinedGeometry.attributes.position.count).toEqual(3);
+  test('addSimulationGeometry works', () => {
+    const scene = new ShadingScene();
+    const positions = [0, 0, 0, 1, 0, 0, 0, 1, 0];
+    const normals = [0, 0, 1, 0, 0, 1, 0, 0, 1];
+    const testGeometry = createTestGeometry(positions, normals);
+    scene.addSimulationGeometry(testGeometry);
+    expect(scene.simulationGeometry?.attributes.position.count).toEqual(3);
+  });
+
+  test('addSolarIrradiance works', () => {
+    const scene = new ShadingScene();
+    const solarIrradiance = {
+      data: [
+        {
+          altitude: 78.28,
+          azimuth: 45.0,
+          radiance: 36.799,
+        },
+        {
+          altitude: 78.28,
+          azimuth: 315.0,
+          radiance: 36.799,
+        },
+      ],
+      metadata: {
+        nside: 4,
+        latitude: 47.21,
+        longitude: 15.86,
+        daylight_timesteps_processed: 4447,
+      },
+    };
+
+    const solarIrradianceSecondTimeStep = {
+      data: [
+        {
+          altitude: 78.28,
+          azimuth: 45.0,
+          radiance: 36.799,
+        },
+        {
+          altitude: 78.28,
+          azimuth: 315.0,
+          radiance: 36.799,
+        },
+      ],
+      metadata: {
+        nside: 4,
+        latitude: 47.21,
+        longitude: 15.86,
+        daylight_timesteps_processed: 4447,
+      },
+    };
+    scene.addSolarIrradiance(solarIrradiance);
+    expect(Array.isArray(scene.solarIrradiance)).toBe(true);
+    expect(scene.solarIrradiance![0].metadata.daylight_timesteps_processed).toEqual(4447);
+
+    const sceneWithTimeSeries = new ShadingScene();
+    sceneWithTimeSeries.addSolarIrradiance([solarIrradiance, solarIrradianceSecondTimeStep]);
+    expect(Array.isArray(scene.solarIrradiance)).toBe(true);
+    expect(scene.solarIrradiance![0].metadata.daylight_timesteps_processed).toEqual(4447);
   });
 });
